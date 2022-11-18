91b09acb6bb1e625b482a8e57814ade34faf4c97
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-18 21:02:19 +0430

    Import mission from Mission

--- src/Components/__test__/Missions.test.js
+++ src/Components/__test__/Missions.test.js
@@ -1,3 +1,4 @@
 import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
+import Missions from '../missions/Missions';
