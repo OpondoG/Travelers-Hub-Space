4b600453a0bf4c82fd0493dcdc4d73bdb3c1927b
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-11-18 21:02:35 +0430

    Update Missions.test.js

--- src/Components/__test__/Missions.test.js
+++ src/Components/__test__/Missions.test.js
@@ -2,3 +2,4 @@ import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import Missions from '../missions/Missions';
+import store from '../../redux/configureStore';
